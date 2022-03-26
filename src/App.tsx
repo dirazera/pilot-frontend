8f107f34f1c794959967256972c97b5e25c86818
Danilo Castro <danilocastro@Danilos-MacBook-Pro.local>
2022-03-26 12:35:07 +0000

    component + some hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,53 @@
-import React from 'react';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
 import logo from './logo.svg';
 
+const name = "App"
+
+type LogoProps = {
+  load: {}
+  title: string
+}
+
+function Logo(props: LogoProps) {
+  const { load, title } = props
+  
+  return <span>{title} | bla: {JSON.stringify(load)}</span>
+}
+
 function App() {
+  // behavior declaration
+  const onClickImage = useCallback((e: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
+    debugger;
+    console.log({
+      alt: e.currentTarget.getAttribute("alt")
+    })
+  }, [])
+  const ref = useRef<HTMLImageElement>(null!)
+  const [age, setAge] = useState<number | null>(null)
+  useEffect(() => {
+    const image = ref.current
+    console.log("UseEffect:", {
+      alt: image.getAttribute("alt"),
+      age,
+      name,
+    })
+    setAge(oldAge => (oldAge ?? 0) % 2)
+    // setAge(0)
+    // setAge(null)
+  }, [age])
+  const load = useMemo(() => ({}), [])
+  
+  // useContext
+  // useReducer
+
+  
+  // return markup
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Logo title="Awesome title" load={load} />
+        <img ref={ref} onClick={onClickImage} src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
